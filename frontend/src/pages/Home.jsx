257ca653ea84a7beeb4f7c7f76e14a26aed4a15d
tina-pbd <tina@pointblankdevelopment.com.au>
2025-06-02 17:05:38 +1000

    working on home page intro

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
+
 export const Home = () => {
+
+    useEffect(() => {
+        document.body.classList.add('home-page');
+
+        return () => {
+            document.body.classList.remove('home-page');
+        }
+    }, []);
+
     return (
         <main>
             <div className="content-section page-intro">
-                <h1>Hi, I'm Tina</h1>
-                <p>A passion front end web developer based in Sydney, Australia.</p>
-                <p>I enjoy taking a slick design and bring it to life with clean quality code.</p>
+                <img className="circle-image" src={`${import.meta.env.BASE_URL}tina-pic-01.png`}  alt="Tina" />
+                <div className="summary">
+                    <h1 className="page-title">Hi, I'm Tina</h1>
+                    <p>A passion front end web developer based in Sydney, Australia.</p>
+                    <p>I enjoy bringing a slick design to life with clean quality code.</p>
+                </div>
             </div>
             <div className="content-section">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat orci quis ornare vulputate. Curabitur bibendum vel nulla id efficitur. Nullam vel nisi sed est tincidunt sollicitudin eget sed purus. Sed sodales ultrices eleifend. Morbi luctus urna sit amet purus tristique, et aliquam erat tempus. Proin at mi eu eros elementum accumsan ac nec ipsum. Integer semper magna a hendrerit fringilla. Donec pharetra dapibus auctor. Fusce placerat non lacus sit amet finibus. Cras et felis a odio venenatis gravida. Fusce et viverra nisl. Proin in orci et elit fermentum rutrum vel eu risus. Ut in nibh turpis. Quisque gravida risus eu ante egestas ultricies. Etiam a condimentum nunc, vitae porta odio. Fusce euismod, sem in malesuada eleifend, justo massa volutpat neque, quis scelerisque eros tellus dignissim lacus.</p>
