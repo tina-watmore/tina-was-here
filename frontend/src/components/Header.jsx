d9fe3165e624df3acf2094b8b3b2d0cae8555a99
tina-pbd <tina@pointblankdevelopment.com.au>
2025-06-02 16:24:44 +1000

    working on mobile navigation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,57 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 export const Header = () => {
+    const [navOpen, setNavOpen] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        if(!navOpen) {
+            document.body.classList.remove('nav-open');
+        } else {
+            document.body.classList.add('nav-open');
+        }      
+    }, [navOpen]);
+
+    // remove nav open classes and reseting navOpen on location change
+    useEffect(() => {
+        return () => {
+            document.body.classList.remove('nav-open');
+            setNavOpen(false);
+        }
+    }, [location]);
+
     return (
-        <header>
-            <Link className="header-logo" to='/'>
-                Tina was here!
-            </Link> 
-            <div className="navbar">
+        <header className={`${navOpen ? 'open' : ''}`}>
+            <div className='header-bar'>
+                <Link className="header-logo" to='/'>
+                    Tina was here!
+                </Link> 
+                <button onClick={() => setNavOpen(!navOpen)} className='navbar-toggle' type='button' aria-label='Toggle navigation'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+            <nav className='navbar'>
                 <ul className="navbar-links" role='list'>
                     <li className="nav-item">
                         <NavLink to="/">Home</NavLink>
                     </li>   
                     <li className="nav-item">
                         <NavLink to="/about">About</NavLink>
-                    </li>                                      
+                    </li>    
+                    <li className="nav-item">
+                        <NavLink to="/experience">Experience</NavLink>
+                    </li>                                   
+                    <li className="nav-item">
+                        <NavLink to="/projects">Hobbie Projects</NavLink>
+                    </li>                                                       
+                    <li className="nav-item">
+                        <NavLink to="/interests">Interests</NavLink>
+                    </li>                                                       
                 </ul>
-            </div>
+            </nav>
         </header>
     );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 export const Home = () => {
     return (
-        <div>
-            <h1>Hi, I'm Tina</h1>
-            <p>A passion front end web developer based in Sydney, Australia.</p>
-            <p>I enjoy taking a slick design and bring it to life with clean quality code.</p>
-        </div>
+        <main>
+            <div className="content-section page-intro">
+                <h1>Hi, I'm Tina</h1>
+                <p>A passion front end web developer based in Sydney, Australia.</p>
+                <p>I enjoy taking a slick design and bring it to life with clean quality code.</p>
+            </div>
+            <div className="content-section">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat orci quis ornare vulputate. Curabitur bibendum vel nulla id efficitur. Nullam vel nisi sed est tincidunt sollicitudin eget sed purus. Sed sodales ultrices eleifend. Morbi luctus urna sit amet purus tristique, et aliquam erat tempus. Proin at mi eu eros elementum accumsan ac nec ipsum. Integer semper magna a hendrerit fringilla. Donec pharetra dapibus auctor. Fusce placerat non lacus sit amet finibus. Cras et felis a odio venenatis gravida. Fusce et viverra nisl. Proin in orci et elit fermentum rutrum vel eu risus. Ut in nibh turpis. Quisque gravida risus eu ante egestas ultricies. Etiam a condimentum nunc, vitae porta odio. Fusce euismod, sem in malesuada eleifend, justo massa volutpat neque, quis scelerisque eros tellus dignissim lacus.</p>
+                <p>Aliquam erat volutpat. Sed fermentum eros sed purus maximus, nec commodo diam euismod. Mauris vel urna a odio luctus euismod quis eget massa. Aenean sit amet dolor at odio rutrum commodo auctor sit amet sapien. Suspendisse massa sem, pretium quis dictum in, vestibulum blandit enim. Aliquam bibendum ex arcu, sit amet congue ante lacinia sed. Sed iaculis commodo dignissim. Sed id egestas urna.</p>
+            </div>
+        </main>
     );
 }
